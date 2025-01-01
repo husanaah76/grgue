fef7390444d01adbbf0a0d864c54707569a545dc
husanaah76 <husanaah76@gmail.com>
2025-01-02 02:11:29 +0800

    Update account.js

--- account.js
+++ account.js
@@ -12,3 +12,25 @@ export const accountLists = [
       "password": "N@ima1990"
     } // for more users just copy paste the form as many as your accounts
 ];
+module.exports = {
+    accountLists
+};
+//========================================
+const uidList = [
+  {
+    "userId": "2oFNRzQeQ505s1UdRItQhxWFBx1",
+    "accessToken": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkJseGtPeW9QaWIwMlNzUlpGeHBaN2JlSzJOSEJBMSJ9.eyJ1c2VySWQiOiIyb0ZOUnpRZVE1MDVzMVVkUkl0UWh4V0ZCeDEiLCJlbWFpbCI6ImFzYWRxYWxieXNla3VtcHVsQGdtYWlsLmNvbSIsInNjb3BlIjoiVVNFUiIsImlhdCI6MTczMTQ1MzYxMCwibmJmIjoxNzMxNDUzNjEwLCJleHAiOjE3NjI1NTc2MTAsImF1ZCI6Ind5bmQtdXNlcnMiLCJpc3MiOiJodHRwczovL3d5bmQuczMuYW1hem9uYXdzLmNvbS9wdWJsaWMifQ.HtfNszSP9Eau6CsUDuOY7uC1NOkP7U1LZewRuxDEkfhLolHkht6zT76pE00FR9_ewzm3u9DUdWNQQn5j-T5RVOLDj3A9_fS4KhIRpLoOnjdX2JwKhbWED2_aylXyZA5xw0WRMPoNTxunPwJFOW_PCx3ulikEZsUJ6pkhQAVfxXCU-fjoVuhMB9GVnUrvG20IjANPBcbfnUcNjCS7Ql79StgvPpX9dvX4J7nSuBlMzV-vi6R8l6OdQk_GVei1W7b_prcjGlZ_SgpRd-Uq9PSKwvAInKsFfNi-VSgwoWvOn1CE5VnoqKU0AZl_Wes0yuHoW_iIIbZ2he5iC4K0g_Wplg"
+  },
+  {
+    "userId": "2oSWHb1DNFJCXXFh9IXGRMd1qTj",
+    "accessToken": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkJseGtPeW9QaWIwMlNzUlpGeHBaN2JlSzJOSEJBMSJ9.eyJ1c2VySWQiOiIyb1NXSGIxRE5GSkNYWEZoOUlYR1JNZDFxVGoiLCJlbWFpbCI6Imhhc2FuaXFhbGJ5Z29tQGdtYWlsLmNvbSIsInNjb3BlIjoiVVNFUiIsImlhdCI6MTczNTI4NDk4NywibmJmIjoxNzM1Mjg0OTg3LCJleHAiOjE3NjYzODg5ODcsImF1ZCI6Ind5bmQtdXNlcnMiLCJpc3MiOiJodHRwczovL3d5bmQuczMuYW1hem9uYXdzLmNvbS9wdWJsaWMifQ.P8Hby43K30XfMhwr75shzUYRxV9x7dSgx2GwpCYpIUbyCfxXnlseeiEywp86BPv_eBgUYHHYKLpAFIsl7ztr3pzzXzT6VcbemFrg8F277NuRU77-UzsfmP-dX3t0xZuM2ZqG_g4_z0eXW_aLozVNijBTWPy6aLLpYucx35xCb6fxlUxDPGdv5k0vSbJ8qxkkDSAmo_ZD_J2HoFV2AxIGqELhtijeLmmvhssYbEdik-71eOB9TJuCOW92FAAUhpBEe700ZumftHb81pBIg6QboBykOQCxjjQHEs0zO0u0dYfOamhaOroGkXTOQWt5XxkIwcarhQ1fVSz_BaQzCewwAA"
+  },
+  {
+    "userId": "2ohT2lRZyZmC0YANluzI37kxRof",
+    "accessToken": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkJseGtPeW9QaWIwMlNzUlpGeHBaN2JlSzJOSEJBMSJ9.eyJ1c2VySWQiOiIyb2hUMmxSWnlabUMwWUFObHV6STM3a3hSb2YiLCJlbWFpbCI6ImVkcmlqYWwyMkBnbWFpbC5jb20iLCJzY29wZSI6IlVTRVIiLCJpYXQiOjE3MzE0NDc2NjYsIm5iZiI6MTczMTQ0NzY2NiwiZXhwIjoxNzYyNTUxNjY2LCJhdWQiOiJ3eW5kLXVzZXJzIiwiaXNzIjoiaHR0cHM6Ly93eW5kLnMzLmFtYXpvbmF3cy5jb20vcHVibGljIn0.WEGT0tFtHxSdhIVNixCQR_vM10f_GJWRt5aMETDH2Xox-UwNcqrJSfsCWDXhe5tGQi8UFi_B40lmwrx2q3rZaMVomv5Z0T1SVA9jjBP-d5onb-8TNg3Y-bckfxG55NGPs0QjOdV37p40L8vDgIYXOly1kYoEpjcGd_vlu7L1qQT_ge1HU5PYBCUNXJD6OPGoufNvj69AxuzeZuIrGeUXPSVXDRavq_5f5ddaRfcDZSe9AWbQoamR0k-TmkC_3ZIRIUSagGd7641GSZoYKSLprbUljTZ6SUiVyb2CepN1IjEi_pdM2sG64NCxRyquJxH6JzursjXASFAQy1YhjLL_Vw"
+  }
+];
+
+module.exports = {
+    uidList
+};
