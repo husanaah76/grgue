09f7d4348226a1bcd278f76bdf5fca68357e7f0b
husanaah76 <husanaah76@gmail.com>
2025-01-02 01:58:33 +0800

    Add files via upload

---
+++ account.js
@@ -0,0 +1,14 @@
+export const accountLists = [
+    {
+      "username": "xxx",
+      "password": "xxx"
+    },
+    {
+      "username": "xxx",
+      "password": "xxx"
+    },
+    {
+      "username": "xxx",
+      "password": "xxx"
+    } // for more users just copy paste the form as many as your accounts
+];
